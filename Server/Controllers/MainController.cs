c72ee15891a7a4ea6cccc537b9428a861ec7d76d
Izzat Aliyev <izzat@macbookpro.speedport.ip>
2024-09-23 19:15:13 +0200

    [update] main controller

--- Server/Controllers/MainController.cs
+++ Server/Controllers/MainController.cs
@@ -7,14 +7,28 @@ using Microsoft.AspNetCore.Mvc;
 [Route("api")]
 public class MainController(ILogger<MainController> logger) : ControllerBase
 {
+    private readonly string path = "../result.json";
     private readonly ILogger<MainController> _logger = logger;
 
-    [HttpGet("result")]
+    [HttpGet]
     public async Task<ActionResult> GetResult()
     {
-        var bytes = await System.IO.File.ReadAllBytesAsync("../result.json");
+        var bytes = await System.IO.File.ReadAllBytesAsync(path);
 
-        this._logger.LogInformation($"[http][{HttpContext.Response.StatusCode}] {HttpContext.Request.Path.Value} [{DateTime.UtcNow}] => Readed result file");
+        this._logger.LogInformation(
+            "[http][{status}] {path} [{date}] => Readed result file",
+            [HttpContext.Response.StatusCode, HttpContext.Request.Path.Value, DateTime.UtcNow]);
         return this.File(bytes, MediaTypeNames.Application.Json);
+    }
+
+    [HttpGet("download")]
+    public async Task<ActionResult> Download()
+    {
+        var bytes = await System.IO.File.ReadAllBytesAsync(path);
+
+        this._logger.LogInformation(
+            "[http][{status}] {path} [{date}] => Readed result file",
+            [HttpContext.Response.StatusCode, HttpContext.Request.Path.Value, DateTime.UtcNow]);
+        return this.File(bytes, MediaTypeNames.Application.Json, Path.GetFileName(path));
     }
 }
--- Server/Program.cs
+++ Server/Program.cs
@@ -29,7 +29,6 @@ public class Program
             app.UseHttpsRedirection();
         }
 
-
         app.UseAuthorization();
 
         app.MapControllers();
